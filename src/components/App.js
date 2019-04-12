f3ac7c2a3d6cc0422b9550e9698bd94c5a850e79
konstantin0s <constantintofan85@gmail.com>
2019-04-12 21:38:47 +0200

    Add axios into battlefield

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
 
 import React from 'react';
 import ZipForm from './ZipForm';
+import { get } from 'axios';
+
 
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            zipcode = '',
+            city: {},
+            dates:[],
+            selectedDate: null
+        };
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.submitZipCode = this.submitZipCode.bind(this);
+    }
+
+    onFormSubmit(zipcode) {
+        get(`http://localhost:3000/weather/${zipcode}`)
+        .then(({ data }) => {
+            const { city, list: dates } = data;
+      
+        this.setState({ zipcode, city, dates, selectedDate: null });
+    });
+    }
 
     render() { 
         return ( 
             <div className="app">
-            <ZipForm />
+            <ZipForm onSubmit={this.onFormSubmit} />
            
             </div>
          );
--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -19,10 +19,21 @@ class ZipForm extends React.Component {
         this.setState({ zipcode: value });
     }
 
+    submitZipCode(e) {
+        e.preventDefault();
+
+        const { zipcode } = this.state;
+        const { onSubmit } = this.props;
+
+        onSubmit(zipcode);
+
+        this.setState({ zipcode: '' });
+    }
+
 render() { 
     return ( 
         <div className="zip-form">
-        <form>
+        <form onSubmit={this.submitZipCode}>
             <label htmlFor="zipcode">Zip Code</label>
             <input value={this.state.zipcode} onInput={this.inputUpdated} className="form-control" type="input" name="zipcode" />
             <button type="button" className="btn btn-success">
