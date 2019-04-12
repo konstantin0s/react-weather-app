a8062c73df779cac7a78ba36fc2b2847068f78aa
konstantin0s <constantintofan85@gmail.com>
2019-04-12 21:16:04 +0200

    Add state and bind to input

--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class ZipForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            zipcode: ''
+        };
+
+        this.inputUpdated = this.inputUpdated.bind(this);
+    }
     
+    
+
+    inputUpdated(e) {
+        const { value } = e.target;
+
+        this.setState({ zipcode: value });
+    }
 
 render() { 
     return ( 
         <div className="zip-form">
         <form>
             <label htmlFor="zipcode">Zip Code</label>
-            <input className="form-control" type="input" name="zipcode" />
+            <input value={this.state.zipcode} onInput={this.inputUpdated} className="form-control" type="input" name="zipcode" />
             <button type="button" className="btn btn-success">
             Get The Forecast!</button>
         </form>
