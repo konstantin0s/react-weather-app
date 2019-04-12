5ae99bde1b28140f8ae45ff1cde13729ab327c40
konstantin0s <constantintofan85@gmail.com>
2019-04-12 20:19:15 +0200

    Yarn add react & react dom package

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 import './styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('Hello, world!');
+class App extends React.Component {
+
+    render() { 
+        return ( 
+            <div className="app">
+            <p>Oracletone rocks!!</p>
+            </div>
+         );
+    }
+}
+ 
+export default App;
+
+const root = document.getElementById('app-container');
+
+ReactDOM.render(<App />, root);
