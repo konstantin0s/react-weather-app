dbffe6e0919d85409466cbfd8730ac525a492adb
konstantin0s <constantintofan85@gmail.com>
2019-04-13 19:00:49 +0200

    Add WeatherList component

---
+++ src/components/WheatherList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class WeatherList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        const { days } = this.props;
+        return (  
+         <div className="wheater-list flex-parent">
+
+         </div>
+        );
+    }
+}
+ 
+export default WeatherList;
